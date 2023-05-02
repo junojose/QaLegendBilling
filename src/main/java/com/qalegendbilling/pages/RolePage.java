73832d1703155c74fea74a20ee34416fc703da0c
junojose <junojose91@gmail.com>
2023-05-02 12:14:22 +0400

    commit

--- src/main/java/com/qalegendbilling/pages/RolePage.java
+++ src/main/java/com/qalegendbilling/pages/RolePage.java
@@ -1,6 +1,8 @@
 package com.qalegendbilling.pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import com.qalegendbilling.utilities.TestHelperUtility;
@@ -12,8 +14,17 @@ public class RolePage extends TestHelperUtility {
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
 	}
+	
+	private final String _addRolesButton="//div[@class='box-tools']//i[@class='fa fa-plus']";  
+	@FindBy(xpath=_addRolesButton)
+	private WebElement addRolesButton;
 	public String rolesPageTitle() {
 		String title = page.getPageTitle(driver);
 		return title;
+	}
+	public AddRolesPage clickOnaddRolesButton()
+	{
+		page.clickOnElement(addRolesButton);
+		return new AddRolesPage(driver);
 	}
 }
